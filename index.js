a96837af3804e5314b7d38865c395c2c37f1e25f
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2021-09-15 21:33:39 +0200

    Refactor

--- index.js
+++ index.js
@@ -270,4 +270,24 @@ function sendErrorMessageToBot(chatId) {
      bot.sendMessage(chatId, constants.errorText);
 };
 
-// module.exports.getInfoWallet = getInfoWallet;
+function sendTotalWallet() {
+     let selectQuery = "select * from scheduler;";
+ 
+     crud.queryDatabase(selectQuery).then(function (result) {
+         for (let row of result.rows) {
+             let json = JSON.stringify(row);
+             let obj = JSON.parse(json);
+             let scheduler = {
+                  userId: obj.user_id,
+                  name: obj.name,
+                  chatId: obj.chat_id
+             };
+             
+             getInfoWallet(scheduler.chatId, scheduler.userId, scheduler.name);
+        }
+    }).catch(function (err) {
+        console.log(`sendTotalWallet: ${err}`);
+    });
+ };
+ 
+ sendTotalWallet();
