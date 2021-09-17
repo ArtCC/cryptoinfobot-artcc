99f7585f46d9b85c6195f6659cc8aa9494065c0f
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2021-09-17 16:00:07 +0200

    Add new info with commands.

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ bot.onText(/^\/alerta (.+)/, (msg, match) => {
           crud.queryDatabase(deleteQuery).then(function (result) {
                bot.sendMessage(chatId, constants.disabledAlertText);
           }).catch(function (err) {
+               console("Error 1");
                sendErrorMessageToBot(chatId);
           });
      } else {
@@ -41,10 +42,12 @@ bot.onText(/^\/alerta (.+)/, (msg, match) => {
                     crud.queryDatabase(insertQuery).then(function (result) {
                          bot.sendMessage(chatId, constants.enabledAlertText);
                     }).catch(function (err) {
+                         console("Error 2");
                          sendErrorMessageToBot(chatId);
                     });
                }
           }).catch(function (err) {
+               console("Error 3");
                sendErrorMessageToBot(chatId);
           });
      }
