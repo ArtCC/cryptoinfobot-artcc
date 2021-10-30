b913ad750f34a404fcf7145df146f1bae40650cb
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2021-10-30 10:03:00 +0200

    Donate text.

--- src/constants.js
+++ src/constants.js
@@ -11,8 +11,8 @@ module.exports = {
     thirdNotificationHour: "22:00",
     esLanguageCode: "es",
     bitcoin: "<b>Bitcoin (BTC): </b>bc1qkrh7mrvxzndek447vgzegk5hsl8l405x4v07xx\n",
-    cardano: "<b>Cardano (ADA): </b>addr1q935l63rku9pdft4z6lj6v7z5hx7qhxxrr2gvcs7naya4ktrfl4z8dc2z6jh294l95eu9fwdupwvvxx5se3pa86fmtvswyvhv4\n",
-    ethereum: "<b>Ethereum (ETH): </b>0xA9334474c66ad4e9f510241D4C5253C71f103424\n",
-    shibaInu: "<b>Shiba Inu (SHIB): </b>0xA9334474c66ad4e9f510241D4C5253C71f103424\n",
-    vechain: "<b>VeChain (VET): </b>0xF49673f8EC4437A1Fa6860508E7a6bDC2d352aF4\n"
+    cardano: "<b>Cardano (ADA): </b>addr1q935l63rku9pdft4z6lj6v7z5hx7qhxxrr2gvcs7naya4ktrfl4z8dc2z6jh294l95eu9fwdupwvvxx5se3pa86fmtvswyvhv4\n\n",
+    ethereum: "<b>Ethereum (ETH): </b>0xA9334474c66ad4e9f510241D4C5253C71f103424\n\n",
+    shibaInu: "<b>Shiba Inu (SHIB): </b>0xA9334474c66ad4e9f510241D4C5253C71f103424\n\n",
+    vechain: "<b>VeChain (VET): </b>0xF49673f8EC4437A1Fa6860508E7a6bDC2d352aF4\n\n"
 };
